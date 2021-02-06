e5b33b34e7d76e9088a0a3ff8c80d17d59e0a7c2
Xiang Xu <samxucmu@gmail.com>
2021-02-06 11:29:43 -0800

    Update new_search_with_train.py

--- new_search_with_train.py
+++ new_search_with_train.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1,0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2"
 import random
+import copy
 import cv2
 import torch
 from new_dataset import myDataset, trainSearchDataset
@@ -15,6 +16,8 @@ from new_utils import visualization, candidate_enumerate_training, reduce_duplic
 from colorama import Fore, Style
 from dicttoxml import dicttoxml
 from new_config import *
+
+import pdb
 
 print(config)
 
@@ -89,27 +92,30 @@ def test(dataloader, model, edge_bin_size):
 
     return total_loss['edge']/batch_count
 
-def train(dataloader, model, edge_bin_size):
+
+
+def train(dataloader, old_model, model, edge_bin_size):
     model.train()
     total_loss = {}
     batch_count = 0
     for idx, data in enumerate(dataloader):
         img = data['img'].to(model.device)
+        next_img = data['img'].to(old_model.device)
         mask = data['mask'].to(model.device)
+        next_mask = data['next_mask'].to(old_model.device)
         corner_gt_mask = data['corner_gt_mask'].to(model.device)
         gt_edge = data['gt_edge'].to(model.device).squeeze(1)
         edge_mask = data['edge_mask'].to(model.device)
+        next_edge_mask = data['next_edge_mask'].to(old_model.device)
 
         optimizer.zero_grad()
 
         loss_dict = {}
+
+
         img_volume = model.imgvolume(img)
-        if use_bin_map:
-            binmap = model.getbinmap(img)
-            binmap_detach = binmap
-        else:
-            binmap = None
-            binmap_detach = None
+        binmap = None
+        binmap_detach = None
         if use_heat_map:
             heatmap = model.getheatmap(img)
             heatmap_detach = heatmap
@@ -123,8 +129,26 @@ def train(dataloader, model, edge_bin_size):
         edge_pred = model.edgeEvaluator(edge_mask, mask, img_volume, corner_pred.detach(),
                                         torch.zeros(edge_mask.shape[0], edge_bin_size, device=model.device),
                                         binmap=binmap_detach, heatmap=heatmap_detach)
+
+
+        next_img_volume = old_model.imgvolume(next_img)
+        binmap = None
+        binmap_detach = None
+        if use_heat_map:
+            next_heatmap = old_model.getheatmap(next_img)
+            next_heatmap_detach = next_heatmap
+        else:
+            next_heatmap = None
+            next_heatmap_detach = None
+        next_corner_pred = old_model.cornerEvaluator(next_mask, next_img_volume, binmap=binmap_detach, heatmap=next_heatmap_detach)
+        next_edge_pred = old_model.edgeEvaluator(next_edge_mask, next_mask, next_img_volume, next_corner_pred.detach(),
+                                    torch.zeros(edge_mask.shape[0], edge_bin_size, device=old_model.device),
+                                    binmap=binmap_detach, heatmap=next_heatmap_detach).detach().cpu()
+        
+        next_edge_pred = next_edge_pred.to(model.device)
+
         corner_l = cornerloss(corner_pred, corner_gt_mask)
-        edge_l = edgeloss(edge_pred, gt_edge)
+        edge_l = edgeloss(edge_pred, (1-gamma) * gt_edge.unsqueeze(1) + gamma * next_edge_pred)
         loss_dict['corner'] = corner_l
         loss_dict['edge'] = edge_l
         if use_cross_loss:
@@ -141,7 +165,7 @@ def train(dataloader, model, edge_bin_size):
             pseudo_corner_score = pseudo_corner_score > 0.8
             pseudo_corner_score = pseudo_corner_score[:,0] & pseudo_corner_score[:,1] # two corners must all be classified as True
             edge_gt_pseudo = ((edge_gt_pseudo > 0.8) & pseudo_corner_score).long()
-            loss_dict['edge_cross'] = edgeloss(edge_pred, edge_gt_pseudo)
+            loss_dict['edge_cross'] = edgeloss2(edge_pred, edge_gt_pseudo)
 
             # corner pseudo
             ### test ###
@@ -185,39 +209,59 @@ def train(dataloader, model, edge_bin_size):
 
 
 class trainThread(threading.Thread):
-    def __init__(self, lock, evaluator, search_evaluator, new_data_memory, dataloader, dataset, testloader):
+    def __init__(self, lock, evaluator, search_evaluator1, search_evaluator2, new_data_memory, dataloader, dataset, testloader):
         super(trainThread, self).__init__()
         self.lock = lock
         self.evaluator = evaluator
-        self.search_evaluator = search_evaluator
+        self.search_evaluator1 = search_evaluator1
+        self.search_evaluator2 = search_evaluator2
         self.new_data_memory = new_data_memory
         self.dataloader = dataloader
         self.dataset = dataset
         self.testloader = testloader
 
     def run(self):
-        print('{}[training thread]{} start'.format(Fore.BLUE, Style.RESET_ALL))
+        print('{}[training thread]{} wait for initial data'.format(Fore.BLUE, Style.RESET_ALL))
         prefix = 0
         testbest = 10
         while True:
+            # pre-load data
+            while len(self.dataset) < 1000:
+                time.sleep(200)
+                print(len(self.new_data_memory))
+                print(len(self.dataset))
+                
+                while len(self.new_data_memory) != 0:
+                    data = self.new_data_memory.pop()
+                    self.dataset.add_processed_data(data)
+
+                continue
+                
+
+            print('{}[training thread]{} start training'.format(
+                    Fore.BLUE, Style.RESET_ALL))
             train_sample = 0
             for _ in range(3):
                 print('{}[training thread]{} New start {}! training with {} samples'.format(
                     Fore.BLUE, Style.RESET_ALL, prefix, len(self.dataset)))
-                train(self.dataloader, self.evaluator, edge_bin_size)
-                self.lock.acquire()
+                train(self.dataloader, copy.deepcopy(self.search_evaluator1), self.evaluator, edge_bin_size)
+                
                 while len(self.new_data_memory) != 0:
                     data = self.new_data_memory.pop()
                     self.dataset.add_processed_data(data)
+
                 # update search evaluator
+                self.lock.acquire()
                 if search_with_evaluator:
-                    self.search_evaluator.load_state_dict(self.evaluator.state_dict())
+                    self.search_evaluator1.load_state_dict(self.evaluator.state_dict())
+                    self.search_evaluator2.load_state_dict(self.evaluator.state_dict())
                 self.lock.release()
+
                 train_sample += len(self.dataset)
                 if train_sample >= MAX_DATA_STORAGE/2:
                     break
 
-            testacc = test(self.testloader, self.evaluator, edge_bin_size)
+            #testacc = test(self.testloader, self.evaluator, edge_bin_size)
             print('{}[training thread]{} update searching evaluator'.format(Fore.BLUE, Style.RESET_ALL))
             print('{}[training thread]{} store weight with prefix={}'.format(Fore.BLUE, Style.RESET_ALL, prefix))
             if os.path.exists(os.path.join(save_path, '{}_{}.pt'.format(prefix-14, 'backbone'))):
@@ -229,11 +273,11 @@ class trainThread(threading.Thread):
             if os.path.exists(os.path.join(save_path, '{}_{}.pt'.format(prefix-14, 'heatmapnet'))):
                 os.remove(os.path.join(save_path, '{}_{}.pt'.format(prefix-14, 'heatmapnet')))
             self.evaluator.store_weight(save_path, prefix)
-            if testacc < testbest:
-                testbest = testacc
-                self.evaluator.store_weight(save_path, 'best')
-                with open(os.path.join(save_path, 'best.txt'), 'w') as f:
-                    f.write('{} {}'.format(testbest, prefix))
+            #if testacc < testbest:
+                #testbest = testacc
+                #self.evaluator.store_weight(save_path, 'best')
+                #with open(os.path.join(save_path, 'best.txt'), 'w') as f:
+                    #f.write('{} {}'.format(testbest, prefix))
             prefix += 1
 
 
@@ -251,6 +295,8 @@ class searchThread(threading.Thread):
         print('{}[searching thread]{} start'.format(Fore.RED, Style.RESET_ALL))
         search_count = 0
         add_count = 0
+        #buffer = []
+
         while True:
             for idx, data in enumerate(self.dataloader):
                 name = data['name'][0]
@@ -279,42 +325,46 @@ class searchThread(threading.Thread):
                         prev_ = prev_candidates[prev_i]
                         if len(prev_.graph.getCorners()) == 0 or len(prev_.graph.getEdges()) == 0:
                             continue
-                        current_candidates.extend(candidate_enumerate_training(prev_, gt_data))
+                        next_candidates = candidate_enumerate_training(prev_, gt_data)
+                        for candidate_ in next_candidates:
+                            #self.lock.acquire()
+                            self.evaluator.get_score(candidate_, all_edge=True)
+                            #self.lock.release()
+                        next_candidates = sorted(next_candidates, key=lambda x:x.graph.graph_score(), reverse=True)
+                        # pick the best as S_{t+1}
+                        next_ = next_candidates[0]
+
+                        prev_corners = prev_.graph.getCornersArray()
+                        prev_edges = prev_.graph.getEdgesArray()
+                        next_corners = next_.graph.getCornersArray()
+                        next_edges = next_.graph.getEdgesArray()
+                        data = self.train_dataset.make_data(prev_.name, prev_corners, prev_edges, next_corners, next_edges)
+
+                        #buffer.append(data)
+                        self.new_data_memory.append(data)
+                        add_count += 1
+
+                        current_candidates.extend(next_candidates)
+
+                    #print('{}[seaching thread]{} {}'.format(Fore.RED, Style.RESET_ALL, len(buffer)))
+                    #if len(buffer) > 100:
+                        #print('{}[seaching thread]{} loading data'.format(Fore.RED, Style.RESET_ALL))
+                        #self.lock.acquire()
+                        #for idxx in range(len(buffer)):
+                            #self.new_data_memory.append(buffer[idxx])
+                        #self.lock.release()
+                        #del buffer
+                        #buffer = []
+                        #print('{}[seaching thread]{} loading complete'.format(Fore.RED, Style.RESET_ALL))
+
+                    current_candidates = reduce_duplicate_candidate(current_candidates)
 
                     if len(current_candidates) == 0:
                         break
-                    current_candidates = reduce_duplicate_candidate(current_candidates)
-
-                    if search_with_evaluator:
-                        for candidate_ in current_candidates:
-                            self.lock.acquire()
-                            self.evaluator.get_score(candidate_, all_edge=True)
-                            self.lock.release()
-                        current_candidates = sorted(current_candidates, key=lambda x:x.graph.graph_score(), reverse=True)
-                        if len(current_candidates) < beam_width:
-                            pick = np.arange(len(current_candidates))
-                        else:
-                            pick = np.arange(beam_width)
-
-                        prev_candidates = [current_candidates[_] for _ in pick]
-                        temp = prev_candidates[0:1]+prev_candidates[-1:]  # best and worst
+                    if len(current_candidates) > beam_width:
+                        prev_candidates = random.sample(current_candidates, beam_width)
                     else:
-                        if len(current_candidates) > beam_width:
-                            prev_candidates = random.sample(current_candidates, beam_width)
-                        else:
-                            prev_candidates = current_candidates
-                        temp = random.choices(prev_candidates, k=2)
-
-                    for candidate_ in temp:
-                        corners_array = candidate_.graph.getCornersArray()
-                        edges_array = candidate_.graph.getEdgesArray()
-                        if corners_array.shape[0] == 0 or edges_array.shape[0] == 0:
-                            continue
-                        data = self.train_dataset.make_data(candidate_.name, corners_array, edges_array)
-                        self.lock.acquire()
-                        self.new_data_memory.append(data)
-                        self.lock.release()
-                        add_count += 1
+                        prev_candidates = current_candidates
 
                     for candidate_ in prev_candidates:
                         candidate_.update()  # update safe_count
@@ -324,22 +374,12 @@ class searchThread(threading.Thread):
                 #    continue
 
                 search_count += 1
-                if (idx+1) % 50 == 0:
+                if (idx+1) % 5 == 0:
                     print('{}[seaching thread]{} Already search {} graphs and add {} '
                           'graphs into database'.format(Fore.RED, Style.RESET_ALL, search_count, add_count))
                     print('{}[seaching thread]{} {} remain in the swap'
                           ' memory'.format(Fore.RED, Style.RESET_ALL, len(self.new_data_memory)))
 
-                #if save_count <= 20:
-                #    save_count += 1
-                #    print('{}[seaching thread]{} Save sample {} into {} with '
-                #          'depth={}'.format(Fore.RED, Style.RESET_ALL, name,
-                #                            str(self.curr_state['save_count'])+'_checkpoint',len(candidate_gallery)-1))
-                #    gt_candidate = Candidate.initial(Graph(gt_corners, gt_edges), name)
-                #    self.evaluator.get_score(gt_candidate)
-                #    save_gallery(candidate_gallery, name,
-                #                 os.path.join(save_path, 'search_demo', str(self.curr_state['save_count'])+'_checkpoint'),
-                #                 best_candidates, gt_candidate)
 
 def save_candidate_image(candidate, base_path, base_name):
     corners = candidate.graph.getCornersArray()
@@ -457,7 +497,8 @@ test_dataset = trainSearchDataset(data_folder, data_scale=data_scale,
                                   edge_strong_constraint=edge_strong_constraint, phase='valid',
                                   corner_bin=False)
 
-search_dataset = myDataset(data_folder, phase='train', edge_linewidth=2, render_pad=-1)
+search_dataset1 = myDataset(data_folder, phase='train', edge_linewidth=2, render_pad=-1)
+search_dataset2 = myDataset(data_folder, phase='train', edge_linewidth=2, render_pad=-1)
 
 train_loader = torch.utils.data.DataLoader(train_dataset,
                                           batch_size=batch_size,
@@ -470,28 +511,46 @@ test_loader = torch.utils.data.DataLoader(test_dataset,
                                           shuffle=True,
                                           num_workers=4)
 
-search_loader = torch.utils.data.DataLoader(search_dataset,
+
+search_loaders1 = torch.utils.data.DataLoader(search_dataset1,
                                           batch_size=1,
                                           shuffle=True,
                                           num_workers=1,
                                           drop_last=False)
 
+search_loaders2 = torch.utils.data.DataLoader(search_dataset2,
+                                          batch_size=1,
+                                          shuffle=True,
+                                          num_workers=1,
+                                          drop_last=False)
+
+
+
 # evaluator_train is used for training
 # evaluator_search is used for searching
 # separate into two modules in order to use multiple threads to accelerate
-evaluator_train = scoreEvaluator_with_train('/local-scratch/fuyang/cities_dataset',
+evaluator_train = scoreEvaluator_with_train('/local-scratch2/xuxiangx/project/datasets/cities_dataset',
                                             backbone_channel=64, edge_bin_size=edge_bin_size,
                                             corner_bin=False)
 if search_with_evaluator:
-    evaluator_search = scoreEvaluator_with_train('/local-scratch/fuyang/cities_dataset',
+    evaluator_search1 = scoreEvaluator_with_train('/local-scratch2/xuxiangx/project/datasets/cities_dataset',
                                                 backbone_channel=64, edge_bin_size=edge_bin_size,
                                                 corner_bin=False)
-    evaluator_search.load_state_dict(evaluator_train.state_dict())
-    evaluator_search.to('cuda:1')
-    evaluator_search.eval()
+    evaluator_search1.load_state_dict(evaluator_train.state_dict())
+    evaluator_search1.to('cuda:1')
+    evaluator_search1.eval()
+
+    evaluator_search2 = scoreEvaluator_with_train('/local-scratch2/xuxiangx/project/datasets/cities_dataset',
+                                                backbone_channel=64, edge_bin_size=edge_bin_size,
+                                                corner_bin=False)
+    evaluator_search2.load_state_dict(evaluator_train.state_dict())
+    evaluator_search2.to('cuda:2')
+    evaluator_search2.eval()
+
 else:
-    evaluator_search = None
-#evaluator_train.load_weight(pretrained_path, 10)
+    evaluator_search1 = None
+    evaluator_search2 = None
+
 
 evaluator_train.to('cuda:0')
 evaluator_train.train()
@@ -499,7 +558,9 @@ evaluator_train.train()
 optimizer = torch.optim.Adam(evaluator_train.parameters(), lr=1e-4)
 cornerloss = nn.L1Loss()
 heatmaploss = nn.MSELoss()
-edgeloss = nn.CrossEntropyLoss(weight=torch.cuda.FloatTensor([1., 3.], device=evaluator_train.device))
+edgeloss = nn.MSELoss()
+edgeloss2 = nn.CrossEntropyLoss(weight=torch.cuda.FloatTensor([1., 3.], device=evaluator_train.device))
+
 
 os.makedirs(save_path, exist_ok=True)
 f = open(os.path.join(save_path, 'config.xml'), 'wb')
@@ -510,14 +571,13 @@ print('save config.xml done.')
 lock = threading.Lock()
 data_memory = []
 
-st = searchThread(lock, evaluator_search, search_loader, data_memory, train_dataset, search_dataset)
-tt = trainThread(lock, evaluator_train, evaluator_search, data_memory, train_loader, train_dataset, test_loader)
+st1 = searchThread(lock, evaluator_search1, search_loaders1, data_memory, train_dataset, search_dataset1)
+st2 = searchThread(lock, evaluator_search2, search_loaders2, data_memory, train_dataset, search_dataset2)
 
-if activate_search_thread:
-    st.start()
+tt = trainThread(lock, evaluator_train, evaluator_search1, evaluator_search2, data_memory, train_loader, train_dataset, test_loader)
+
+# activate_search_thread:
+st1.start()
+st2.start()
 tt.start()
-
-if activate_search_thread:
-    st.join()
-tt.join()
 
