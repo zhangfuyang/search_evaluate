295bdd62fa744c29cdf8659bc2a4f1d2b1f94442
Xiang Xu <samxucmu@gmail.com>
2021-02-11 05:20:43 -0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -53,23 +53,21 @@ class ReplayMemory(object):
 ############
 class Agent():
 
-    def __init__(self, policy_net, target_net, ground_truth):
+    def __init__(self, search_dataset, ground_truth):
         self.img_transforms = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-        self.policy_net = policy_net
-        self.target_net = target_net
         self.ground_truth = ground_truth
+        self.search_dataset = search_dataset
 
-    def select_action(self, state, epsilon, use_target_net=True):
-        if use_target_net:
-            model = self.target_net
-        else:
-            model = self.policy_net
+    def select_action(self, state, epsilon, network):
+        model = network
         # rank value function from a set of heurastic actions 
         with torch.no_grad():
+            graph_data = self.search_dataset.getDataByName(state.name)
+            gt_data = graph_data['gt_data']
             initial_candidate = Candidate.initial(Graph(state.corners, state.edges), state.name)
-            next_candidates = candidate_enumerate_training(initial_candidate, 1)
+            next_candidates = candidate_enumerate_training(initial_candidate, 1, gt_data)
             next_candidates = reduce_duplicate_candidate(next_candidates)
             model.get_score_list(next_candidates, all_edge=True)
             next_candidates = sorted(next_candidates, key=lambda x:x.graph.graph_score(), reverse=True) # descending scores
@@ -85,15 +83,12 @@ class Agent():
         next_state = State(state.name, next_corners, next_edges)
         return next_state 
 
-    def max_q_action(self, state, use_target_net=True):
-        next_state = self.select_action(state, epsilon=-1, use_target_net=use_target_net)
+    def max_q_action(self, state, network):
+        next_state = self.select_action(state, -1, network)
         return next_state
 
-    def value_func(self, state, num_edges, use_policy_net=True, prev_state=None, prev_edge_idx=None):
-        if use_policy_net:
-            model = self.policy_net
-        else:
-            model = self.target_net
+    def value_func(self, state, num_edges, network, prev_state=None, prev_edge_idx=None):
+        model = network
 
         # Image feature 
         img = skimage.img_as_float(plt.imread(os.path.join(config['data_folder'], 'rgb', state.name+'.jpg')))
@@ -112,12 +107,13 @@ class Agent():
             heatmap = None
             heatmap_detach = None
 
-        
+
         # Full mask
         mask = render(state.corners, state.edges, render_pad=-1, scale=config['data_scale'])
         mask = torch.FloatTensor(mask).unsqueeze(0).to(model.device)
 
         edge_masks = []
+        coords = []
         prev_edge_shared_idx = []
 
         # Use previous state edge locations
@@ -152,6 +148,7 @@ class Agent():
                     edge_mask = render(state.corners, new_id[np.newaxis,:], render_pad=-1, scale=config['data_scale'])[0]
                     edge_masks.append(edge_mask)
                     prev_edge_shared_idx.append(idx)
+                pdb.set_trace()
 
         # Random sampled edge masks
         else:
@@ -163,8 +160,11 @@ class Agent():
             for id_ in edge_idx:
                 edge_mask = render(state.corners, state.edges[[id_]], render_pad=-1, scale=config['data_scale'])[0]
                 edge_masks.append(edge_mask)
+                coord = np.round(state.corners[state.edges[id_]]).astype(np.int)
+                coords.append(coord)
 
         edge_masks = np.array(edge_masks)
+        coords = np.array(coords)  # (1, 2, 2)
         edge_masks = torch.FloatTensor(edge_masks).unsqueeze(1).to(model.device)
         assert(edge_masks.shape[0] > 0)
         
@@ -196,11 +196,13 @@ class Agent():
             out_data['edge_idx'] = None
         out_data['prev_edge_shared_idx'] = prev_edge_shared_idx
         out_data['heatmap'] = heatmap
+        out_data['coords'] = coords
         out_data['edge_masks'] = edge_masks
         out_data['name'] = state.name
         return out_data
 
     def compute_loss(self, state_value, reward, next_state_value=None):
+        total_loss = {}
         if next_state_value is not None:
             # Find removed and shared Edge
             assert state_value['edge_batch_pred'].shape[0] == len(state_value['edge_idx'])
@@ -224,8 +226,7 @@ class Agent():
             expected_edge_values = (next_edges_value_ * config['gamma']) + edge_rewards * (1-config['gamma'])
       
             # Edge loss 
-            edge_l = F.smooth_l1_loss(prev_edges_value, expected_edge_values.detach())
-            total_loss['edge'] = edge_l.item()
+            edge_loss = F.smooth_l1_loss(prev_edges_value, expected_edge_values.detach())
         
             # Corner loss
             prev_corner_value = state_value['corner_pred']
@@ -233,21 +234,22 @@ class Agent():
             gt_mask = (corner_rewards>0).detach()
             next_corner_value = next_state_value['corner_pred'].detach().cpu().to(prev_corner_value.device) * gt_mask.reshape(1,1,256,256)
             expected_corner_values = (next_corner_value * config['gamma']) + corner_rewards * (1-config['gamma'])
-            corner_l = F.smooth_l1_loss(prev_corner_value, expected_corner_values.detach())
-            total_loss['corner'] = corner_l.item()
-
+            corner_loss = F.smooth_l1_loss(prev_corner_value, expected_corner_values.detach())
+            pdb.set_trace()
         # No value function 
         else:
             # Edge loss 
             prev_edges_value = state_value['edge_batch_pred']
-            edge_rewards = torch.FloatTensor(reward['edge_gt'][state_value['edge_idx']]).to(prev_edges_value.device).unsqueeze(1)
-            edge_l = F.smooth_l1_loss(prev_edges_value, edge_rewards.detach())
+            edgeloss = nn.CrossEntropyLoss(weight=torch.cuda.FloatTensor([1., 3.]))
+            edge_rewards = torch.LongTensor(reward['edge_gt'][state_value['edge_idx']]).to(prev_edges_value.device)
+            edge_l = edgeloss(prev_edges_value, edge_rewards)
             total_loss['edge'] = edge_l.item()
 
             # Corner loss
+            cornerloss = nn.L1Loss()
             prev_corner_value = state_value['corner_pred']
             corner_rewards = torch.FloatTensor(reward['corner_gt']).to(prev_corner_value.device).reshape(1,1,256,256)
-            corner_l = F.smooth_l1_loss(prev_corner_value, corner_rewards.detach())
+            corner_l = cornerloss(prev_corner_value, corner_rewards.detach())
             total_loss['corner'] = corner_l.item()
 
         # Heatmap loss 
